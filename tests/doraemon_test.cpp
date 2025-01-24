e941ec42b793fb9d07725687ee1c774cc64ff6da
lihanhui <hanhui.li@qq.com>
2025-01-24 15:52:34 +0800

    ubuntu compile

--- tests/doraemon_test.cpp
+++ tests/doraemon_test.cpp
@@ -2,7 +2,11 @@
 
 #include "gtest/gtest.h"
 
+#include "doraemon/ipc/shared_memory.h"
+
 int main(int argc, char **argv) {
+  doraemon::SharedMemory* sm = new doraemon::SharedMemory("example", 1024, doraemon::SharedMemory::AccessMode::AM_WRITE);
+  sm->begin();
   ::testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
 }
